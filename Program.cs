b5730a63823784c168c552ee9354aebe80590dc8
Jude Davis <juducation@outlook.com>
2022-07-05 22:17:35 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Prompt the user to enter a number.  Use a for-loop to calculate the sum of all the numbers from 1 to the number entered.
+string repeatInput = "";
+
+do
+{
+    int sum = 0;
+    Console.WriteLine("Hello, please enter a number:");
+    int inputA = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i <= inputA; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine(sum);
+    Console.WriteLine("Would you like to continue? y/n:");
+    repeatInput = Console.ReadLine();
+} while (repeatInput.ToLower() == "y");
+Console.WriteLine("Goodbye!");
